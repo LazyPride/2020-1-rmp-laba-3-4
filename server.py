cc5a11a93aade63e706239accdabed94710ab041
Ivan Kovalenko <s1ckretbox@gmail.com>
2020-11-15 23:56:32 +0200

    feat(server): add template rendering
    Signed-off-by: Ivan Kovalenko <s1ckretbox@gmail.com>

--- server.py
+++ server.py
@@ -1,10 +1,18 @@
 from flask import Flask
+from flask import json
+from flask import render_template 
 
 app = Flask(__name__)
 @app.route('/')
-
 def hello_world():
-    return 'Hello World!'
+    # TODO: Handle exception if file not found
+    file = open("./cfg/rooms.json")
+    fileStr = file.read()
+    # TODO Logging without print method
+    print(fileStr)
+    fileJSON = json.loads(fileStr)
+    print(fileJSON)
+    return render_template("controls.html", rooms=fileJSON)
 
 if __name__ == '__main__':
     app.run(debug=True) 
