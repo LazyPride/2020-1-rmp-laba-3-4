bce0888075d560543ec455b60ba7261696f96c37
Ivan Kovalenko <s1ckretbox@gmail.com>
2020-12-19 22:54:34 +0200

    feat(server): add SocketWrapper
    Signed-off-by: Ivan Kovalenko <s1ckretbox@gmail.com>

---
+++ SocketWrapper.py
@@ -0,0 +1,20 @@
+from flask import Flask
+from flask import json
+from flask_socketio import SocketIO
+
+class SocketWrapper(object):
+    def __new__(cls):
+        if not hasattr(cls, 'instance'):
+            cls.instance = super(SocketWrapper, cls).__new__(cls)
+        return cls.instance
+        
+    def tie(self, socket):
+        self.socket = socket
+        
+    def emmit_update(self, json):
+        # TODO: Add logging
+        self.socket.emmit('update', json)
+    
+    def emmit_confirm(self, json):
+        # TODO: Add logging
+        self.socket.emmit('update-confirm', json)
