f404e3221ab449548e8e6ae826b9a3441cbdbf88
Ivan Kovalenko <s1ckretbox@gmail.com>
2020-12-20 15:03:44 +0200

    feat(light_bulb): add updating values
    Signed-off-by: Ivan Kovalenko <s1ckretbox@gmail.com>

--- light_bulb.py
+++ light_bulb.py
@@ -12,3 +12,20 @@ class LightBulb(Component, Switchable):
         print("\t id:" + self.id)
         print("\t type:" + self.type)
         print("\t is_on:" + str(self.is_on))
+    
+    def turnOn(self):
+        self.is_on = 1
+        SocketWrapper().emmit_update_var(self.id, "is_on", self.is_on)
+        
+    def turnOff(self):
+        self.is_on = 0
+        SocketWrapper().emmit_update_var(self.id, "is_on", self.is_on)
+        
+    def update_value(self, var_name, var):
+        if var_name == 'is_on':
+            if var == 1:
+                self.turnOn()
+            elif var == 0:
+                self.turnOff()
+        
+            
