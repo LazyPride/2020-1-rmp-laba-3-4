5e920291eeebce6fa56b37900b83443f7e504616
Ivan Kovalenko <s1ckretbox@gmail.com>
2020-12-20 12:15:34 +0200

    feat(server): rewrite Heater component
    Signed-off-by: Ivan Kovalenko <s1ckretbox@gmail.com>

--- heater.py
+++ heater.py
@@ -1,35 +1,46 @@
 import threading
 from SocketWrapper import SocketWrapper
+from ABCs import *
 
-class Heater:
-    def __init__(self, roomName, isHeatingOn, curTemp, maxTemp):
-        self.roomName = roomName
-        self.curTemp = curTemp
-        self.maxTemp = maxTemp
-        self.isOn = isHeatingOn
+class Heater(Component, Switchable, Adjustable, Bounded):
+    def __init__(self, is_on, min, max, now):
+        super().__init__()
+        self.type = 'Heater'
+        self.is_on = is_on
+        self.min = min
+        self.max = max
+        self.now = now
+        
+    def info(self):
+        print("[Component]:")
+        print("\t type:" + self.type)
+        print("\t is_on:" + str(self.is_on))
+        print("\t min:" + str(self.min))
+        print("\t max:" + str(self.max))
+        print("\t now:" + str(self.now))
     
     def start(self, updateTime):
         threading.Timer(updateTime, self.start, [updateTime],{} ).start()
-            if self.isOn == 1:
-                if int(self.curTemp) >= int(self.maxTemp):
-                    self.turnOff()
-                else:
-                    if int(self.curTemp) < 100:
-                        self.curTemp = self.curTemp + 1
+        if self.is_on == 1:
+            if int(self.now) >= int(self.max):
+                self.turnOff()
             else:
-                if self.curTemp > 0:
-                    self.curTemp = self.curTemp - 1
-            self.__emmit__("temperature", self.curTemp)
+                if int(self.now) < 100:
+                    self.now = self.now + 1
+        else:
+            if self.now > 0:
+                self.now = self.now - 1
+        self.__emmit__("temperature", self.now)
 
     def turnOn(self):
-        self.isOn = 1
-        print("The heater in {} is On({}) to {} degrees".format(self.roomName, self.isOn, self.maxTemp))
-        self.__emmit__("isHeatingOn", self.isOn)
+        self.is_on = 1
+        print("The heater in {} is On({}) to {} degrees".format(self.roomName, self.is_on, self.max))
+        self.__emmit__("isHeatingOn", self.is_on)
         
     def turnOff(self):
-        self.isOn = 0
-        print("The heater in {} is Off({})".format(self.roomName, self.isOn))
-        self.__emmit__("isHeatingOn", self.isOn)
+        self.is_on = 0
+        print("The heater in {} is Off({})".format(self.roomName, self.is_on))
+        self.__emmit__("isHeatingOn", self.is_on)
         
     def update(self, jsonFile):
         if jsonFile["roomName"] == self.roomName:
@@ -38,8 +49,8 @@ class Heater:
                     self.turnOn()
                 else:
                     self.turnOff()
-            elif jsonFile["varName"] == "maxTemperature":
-                self.maxTemp = jsonFile["varValue"]
+            elif jsonFile["varName"] == "maxerature":
+                self.max = jsonFile["varValue"]
 
     def __emmit__(self, varName, varValue):
         msg = {}
