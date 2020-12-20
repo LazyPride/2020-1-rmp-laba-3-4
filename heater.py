0f9c6eb2af5e49eb9962e15ba6fdfdc18c77b520
Ivan Kovalenko <s1ckretbox@gmail.com>
2020-12-20 09:59:23 +0200

    refactor(server)!: rewrite heater logic
    Signed-off-by: Ivan Kovalenko <s1ckretbox@gmail.com>

--- heater.py
+++ heater.py
@@ -1,43 +1,50 @@
 import threading
+from SocketWrapper import SocketWrapper
 
 class Heater:
-    def __init__(self, roomName, curTemp, maxTemp):
+    def __init__(self, roomName, isHeatingOn, curTemp, maxTemp):
         self.roomName = roomName
         self.curTemp = curTemp
         self.maxTemp = maxTemp
-        self.isOn = 0
-        
+        self.isOn = isHeatingOn
+    
+    def start(self, updateTime):
+        threading.Timer(updateTime, self.start, [updateTime],{} ).start()
+            if self.isOn == 1:
+                if int(self.curTemp) >= int(self.maxTemp):
+                    self.turnOff()
+                else:
+                    if int(self.curTemp) < 100:
+                        self.curTemp = self.curTemp + 1
+            else:
+                if self.curTemp > 0:
+                    self.curTemp = self.curTemp - 1
+            self.__emmit__("temperature", self.curTemp)
+
     def turnOn(self):
         self.isOn = 1
         print("The heater in {} is On({}) to {} degrees".format(self.roomName, self.isOn, self.maxTemp))
+        self.__emmit__("isHeatingOn", self.isOn)
         
     def turnOff(self):
         self.isOn = 0
         print("The heater in {} is Off({})".format(self.roomName, self.isOn))
+        self.__emmit__("isHeatingOn", self.isOn)
+        
+    def update(self, jsonFile):
+        if jsonFile["roomName"] == self.roomName:
+            if jsonFile["varName"] == "isHeatingOn":
+                if jsonFile["varValue"] == 1:
+                    self.turnOn()
+                else:
+                    self.turnOff()
+            elif jsonFile["varName"] == "maxTemperature":
+                self.maxTemp = jsonFile["varValue"]
 
-heater_map = {}
-
-def heaterInit(json):
-    global heater_map
-    for room in json:
-        heater_map[room["roomName"]] = Heater(room["roomName"], room["temperature"], room["maxTemperature"])
-        if room["isHeatingOn"] == 1:
-            heater_map[room["roomName"]].turnOn()
-        else:
-            heater_map[room["roomName"]].turnOff()
-
-            
-
-def heaterUpdate(json):
-    global heater_map
-    print(heater_map)
-    if json["varName"] == "isHeatingOn":
-        if json["varValue"] == 1:
-            print("I AM ON")
-            heater_map[json["roomName"]].turnOn()
-        else:
-            print("I AM OFF")
-            heater_map[json["roomName"]].turnOff()
-    elif json["varName"] == "maxTemperature":
-        heater_map[json["roomName"]].maxTemp = json["varValue"]
-    
+    def __emmit__(self, varName, varValue):
+        msg = {}
+        msg["roomName"] = self.roomName
+        msg["varName"] = varName
+        msg["varValue"] = varValue
+        json_msg = json.dumps(msg)
+        SocketWrapper().update(json_msg)
