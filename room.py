11028e7412f85bbc96913ddac1e5fc02048b61e2
Deadly-Sin <myroshnyk.yy@gmail.com>
2020-12-20 15:24:02 +0200

    Revert " moved tkinter from 2 LR"
    This reverts commit 631c1c659c936660f3d5a209b4ec087ca4c7a63c.

--- room.py
+++ room.py
@@ -9,27 +9,27 @@ from HeaterWrapper import HeaterWrapper
 
 
 class Room(object):
-
+    
     def __init__(self, json_cfg):
         self.config = json_cfg
         self.room_name = self.config['room_name']
         self.components = []
         self.__parse_components(self.config['components'])
-
+        
     def updateValue(self, jsonFile):
         pass
-
+        
     def getConfig(self):
         return self.config
-
+        
     def info(self):
         print("[Room]:")
         print("\t name:" + self.config['room_name'])
-        for component in self.components:
+        for component in self.components:  
             component.info()
 
     def __parse_components(self, components):
-        for component in components:
+        for component in components:  
             entity = Component()
             if component['type'] == 'heating':
                 entity = Heater(component['is_on'],
@@ -41,5 +41,5 @@ class Room(object):
                 entity = LightBulb(component['is_on'])
             elif component['type'] == 'light_bulb_brightness':
                 entity = LightBulbBrightness(component['is_on'], component['now'])
-
+            
             self.components.append(entity)
--- server.py
+++ server.py
@@ -1,39 +1,29 @@
 from flask import Flask
 from flask import json
-from flask import render_template
+from flask import render_template 
 from flask_socketio import SocketIO
-from tkinter import *
-import matplotlib.pyplot as plt
-import matplotlib.animation as animation
-from datetime import datetime
-from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 
 from heater import *
 from SocketWrapper import SocketWrapper
-from House import House
-import tkinterClient
+from house import House
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 's1ckret'
 socketio = SocketIO(app)
 
-
 @app.route('/')
 def hello_world():
     return render_template("controls.html", rooms=fileJSON)
 
-
 @socketio.on('message')
 def handle_message(message):
     print('received message: ' + message)
-
 
 @socketio.on('sync')
 def handle_my_custom_event():
     print('Start synchronizing...')
     SocketWrapper().emmit('sync', House().getConfig())
     print('Synchronizing completed.')
-
 
 @socketio.on('update')
 def handle_message(json_msg):
@@ -43,15 +33,16 @@ def handle_message(json_msg):
     SocketWrapper().emmit_update(json_msg)
     print('Emit update-confirm to a client: ' + json_msg)
 
-
 @socketio.on('update-confirm')
 def handle_message(json):
     print('Receive update-confirm from a client: ' + json)
 
-
 if __name__ == '__main__':
+
     SocketWrapper().tie(socketio)
     House().setConfig("./cfg/rooms.json")
-    tkinterClient.init()
-    # House().info()
-    # socketio.run(app, debug=True)
+    House().info()
+    #socketio.run(app, debug=True) 
+    
+    
+
