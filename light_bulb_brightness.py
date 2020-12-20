97b98adeb387c1fbb484d8545bbc449549d9a0ca
Ivan Kovalenko <s1ckretbox@gmail.com>
2020-12-20 12:12:28 +0200

    feat(server): add Light Bulb Brightness component
    Signed-off-by: Ivan Kovalenko <s1ckretbox@gmail.com>

---
+++ light_bulb_brightness.py
@@ -0,0 +1,19 @@
+from ABCs import *
+
+class LightBulbBrightness(Component, Switchable, Adjustable, Bounded):
+    def __init__(self, is_on, now):
+        super().__init__()
+        self.type = 'light_bulb_brightness'
+        self.is_on = is_on
+        self.min = 0
+        self.max = 100
+        self.now = now
+        
+    def info(self):
+        print("[Component]:")
+        print("\t type:" + self.type)
+        print("\t is_on:" + str(self.is_on))
+        print("\t min:" + str(self.min))
+        print("\t max:" + str(self.max))
+        print("\t now:" + str(self.now))
+    
